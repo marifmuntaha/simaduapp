feb38ee0e6949522b2f68b1b189cfe62877af14d
server-ydh <marifmuntaha@gmail.com>
2024-06-12 16:37:19 +0700

    120620241636

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\YearController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function (){
     Route::post('login', AuthController::class. '@login');
-    Route::post('logout', AuthController::class. '@logout');
     Route::post('reset-password', AuthController::class. '@resetPassword');
     Route::post('change-password', AuthController::class. '@changePassword');
 });
@@ -15,4 +15,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::group(['prefix' => 'master'], function (){
         Route::apiResource('year', YearController::class);
     });
+    Route::apiResource('user', UserController::class);
+    Route::post('auth/logout', AuthController::class. '@logout');
 });
